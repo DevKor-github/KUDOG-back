116807748183541adb5c0337ae152985b56ba67e
overthestream <njhnjh02@naver.com>
2023-09-27 14:24:25 +0900

    feat: user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import CategoryPerUser from './categoryPerUser.entity';
 
 @Entity('users')
@@ -8,6 +8,30 @@ class User {
 
   @OneToMany(() => CategoryPerUser, (categoryPerUser) => categoryPerUser.user)
   categoryPerUsers: CategoryPerUser[];
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  subscribeMail: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  refreshToken: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  studentId: number;
+
+  @Column()
+  grade: number;
+
+  @Column()
+  major: string;
 }
 
 export default User;
