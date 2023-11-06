88c8b188c176e34f02534ae0bc67690e64cace8c
overthestream <njhnjh02@naver.com>
2023-11-07 01:20:47 +0900

    feat: fetch notices

--- src/fetch/fetch.service.ts
+++ src/fetch/fetch.service.ts
@@ -3,6 +3,7 @@ import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category, Notice } from 'src/entities';
 import { Repository } from 'typeorm';
+import noticeFetcher from './fetch';
 
 @Injectable()
 export class FetchService {
@@ -14,14 +15,35 @@ export class FetchService {
   ) {}
   private readonly logger = new Logger(FetchService.name);
 
-  @Cron('0 12 1 * * *', { timeZone: 'Asia/Seoul' })
+  @Cron('0 0 18 * * *', { timeZone: 'Asia/Seoul' })
   async fetchNotices() {
-    const categories = await this.categoryRepository.find();
-    console.log(categories);
-    //Promise.all(
-    //  categories.map(async (category) => await this.fetchNotice(category)),
-    //);
+    const categories = await this.categoryRepository.find({
+      relations: ['provider'],
+    });
+
+    Promise.all(
+      categories.map(async (category) => await this.fetchNotice(category)),
+    );
   }
 
-  async fetchNotice(category: Category) {}
+  async fetchNotice(category: Category) {
+    const dto = {
+      provider: category.provider.name,
+      url: category.url,
+      categoryId: category.id,
+    };
+    const dtos = await noticeFetcher(dto);
+    Promise.all(
+      dtos.map(async (data) => {
+        await this.noticeRepository.insert({
+          title: data.title,
+          content: data.content,
+          writer: data.writer,
+          date: data.writtenDate,
+          url: data.page.url,
+          category,
+        });
+      }),
+    );
+  }
 }
