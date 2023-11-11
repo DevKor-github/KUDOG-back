70266c872667e8bba267fc39a5d7671f7fff1350
leestar0629 <yale901@naver.com>
2023-11-11 18:43:30 +0900

    feat:swagger 추가 docs:리턴 타입 number에서 string으로 변경

--- src/category/category.contoller.ts
+++ src/category/category.contoller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CategoryService } from './category.service';
-import {
-} from '@nestjs/swagger';
+import { ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
+import { Category } from 'src/entities';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
   @Get('/:id/categories')
-  async getcategories(@Param('id') providerId: number) {
+  @ApiOperation({
+    summary: ' provider id로 해당 category 가져오기 ',
+    description: '해당 category 가져옴',
+  })
+  @ApiCreatedResponse({ description: 'category 반환 ', type: Category })
+  async getcategories(@Param('id') providerId: string) {
     try {
-      return await this.categoryService.findById(providerId);
+      return await this.categoryService.getcategories(providerId);
     } catch (err) {
       return err;
     }
