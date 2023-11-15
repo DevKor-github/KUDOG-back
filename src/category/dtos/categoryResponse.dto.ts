29ba7ef5562e4baea5312c9c7c5114b59c0ec5db
leestar0629 <yale901@naver.com>
2023-11-15 16:33:03 +0900

    feat:CategoryResponse함수 Dto 생성

--- src/category/dtos/categoryResponse.dto.ts
+++ src/category/dtos/categoryResponse.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CategoryReponseDto {
-  @ApiProperty({
-    example: '컴퓨터학부',
-  })
-  category: string;
+  @ApiProperty({description:'category의 id',example:1})
+  id: number;
+  @ApiProperty({description:'category의 이름',example:['컴퓨터학부','산업경영공학부']})
+  name: string;
+  @ApiProperty({description:'category의 url',example:'https:info.korea.ac.kr/info/board/notice_under.do'})
+  url: string;
 }
