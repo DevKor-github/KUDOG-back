3270427333cd91f9fae414e4b2e3e116c193f842
overthestream <njhnjh02@naver.com>
2023-11-21 01:16:53 +0900

    feat: exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionFilter } from './all.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   const docs = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, docs);
 
+  app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(3050);
 }
 bootstrap();
