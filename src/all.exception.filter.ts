7667b8f87f6e37dbb2debfae63bbad654808a4c2
overthestream <njhnjh02@naver.com>
2024-01-15 18:34:36 +0100

    Add customLogger to AllExceptionFilter

--- src/all.exception.filter.ts
+++ src/all.exception.filter.ts
@@ -5,9 +5,12 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { Logger } from './logger';
 
 @Catch(HttpException)
 export class AllExceptionFilter implements ExceptionFilter {
+  customLogger: Logger;
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -15,6 +18,11 @@ export class AllExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
     const message = exception.message;
     const name = exception.name;
+
+    if (status >= 500) {
+      console.error(exception.message);
+      console.error(exception.stack);
+    }
 
     response.status(status).json({
       status,
