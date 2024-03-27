be846eb96ed956e9e6a8f0e8e3d6cb66b228dca6
overthestream <njhnjh02@naver.com>
2024-03-27 18:21:50 +0900

    fix module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from 'src/entities';
+import {
+  Category,
+  CategoryPerSubscribeBoxEntity,
+  ScrapBox,
+  SubscribeBox,
+} from 'src/entities';
 import { CategoryController } from './category.contoller';
 import { CategoryService } from './category.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Category,
+      CategoryPerSubscribeBoxEntity,
+      SubscribeBox,
+      ScrapBox,
+    ]),
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
