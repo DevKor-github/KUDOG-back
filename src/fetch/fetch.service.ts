e69886728d7b2bc3da5b18104b6a921cbdf924c2
overthestream <njhnjh02@naver.com>
2024-04-02 15:49:37 +0900

    delete comments

--- src/fetch/fetch.service.ts
+++ src/fetch/fetch.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Category, KudogUser, Notice } from 'src/entities';
+import { Category, Notice } from 'src/entities';
 import { Repository } from 'typeorm';
 import noticeFetcher from './fetch';
 import { ChannelService } from 'src/channel/channel.service';
-import { MailService } from 'src/mail/mail.service';
 
 @Injectable()
 export class FetchService {
@@ -14,10 +13,7 @@ export class FetchService {
     private readonly noticeRepository: Repository<Notice>,
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
-    @InjectRepository(KudogUser)
-    private readonly kudogUserRepository: Repository<KudogUser>,
     private channelService: ChannelService,
-    private mailService: MailService,
   ) {}
   private readonly logger = new Logger(FetchService.name);
 
@@ -53,58 +49,6 @@ export class FetchService {
     await this.channelService.sendMessageToKudog(message);
   }
 
-  /*
-  async sendMail(
-    noticeData: {
-      category: string;
-      categoryId: number;
-      notices: { title: string; url: string; content: string }[];
-    }[],
-  ) {
-    const users = await this.kudogUserRepository.find({
-      where: { subscribing: true },
-      relations: ['mail', 'categoryPerUsers'],
-    });
-    await Promise.all(
-      users.map(async (user) => {
-        let html = '';
-        const noticesToSend = noticeData.filter(
-          (data) =>
-            user.categoryPerUsers.filter(
-              (categories) => categories.category_id == data.categoryId,
-            ).length > 0,
-        );
-        const today = new Date().toISOString().slice(0, 10);
-        html = html.concat(`<html><head></head><body>`);
-        html =
-          html.concat(`	<h2 style="width: 100%; margin: 0 auto; text-align: center;">${user.name}님을 위한 공지사항입니다.</h2>
-	<div style="width: 95%; text-align: right; margin: 0 auto;">2023-02-12</div>
-	<div style="width: 95%; margin: 0 auto;">`);
-        noticesToSend.forEach((data) => {
-          html = html.concat(
-            `<h4 style="text-align: left;">${data.category}</h4>`,
-          );
-          html = html.concat(
-            data.notices
-              .map((notice) => {
-                return `<p>${notice.content}</p>`;
-              })
-              .join('<hr style="height: 2px; width: 100%;">'),
-          );
-        });
-        html = html.concat(`	</div>
-</body>
-</html>`);
-        if (user.mail.subscriberEmail != 'admin')
-          await this.mailService.sendMail(
-            user.mail.subscriberEmail,
-            `[KUDOG] ${user.name}님을 위한 ${today}의 공지사항`,
-            html,
-          );
-      }),
-    );
-  }
-*/
   async fetchNotice(category: Category) {
     const dto = {
       provider: category.provider.name,
