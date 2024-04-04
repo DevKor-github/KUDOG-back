31f7c51c727049887b31ec38da3547a7f070df61
overthestream <njhnjh02@naver.com>
2024-04-04 16:16:30 +0900

    delete 'category' domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CategoryModule } from './category/category.module';
 import { ConfigModule } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailModule } from './mail/mail.module';
@@ -39,13 +38,11 @@ import { UsersModule } from './users/users.module';
     ScheduleModule.forRoot(),
     MailModule,
     AuthModule,
-    CategoryModule,
     FetchModule,
     NoticeModule,
     ProviderModule,
     ChannelModule,
     UsersModule,
-    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
