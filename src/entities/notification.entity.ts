f2ced680427a048a51f78de98b151bf627218bf1
daheekim01 <ekgml5334@gmail.com>
2024-04-09 05:30:32 +0900

    kudogUser 참조할 수 있도록 수정

--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -4,7 +4,9 @@ import {
   PrimaryGeneratedColumn,
   JoinColumn,
   ManyToOne,
+  CreateDateColumn,
 } from 'typeorm';
+import { KudogUser } from 'src/entities';
 import { NotificationToken } from './notification-token.entity';
 
 @Entity({ name: 'notifications' })
@@ -12,21 +14,26 @@ export class Notifications {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @JoinColumn({ name: 'notification_token_id', referencedColumnName: 'id' })
+  /* @JoinColumn({ name: 'notification_token_id', referencedColumnName: 'id' })
   @ManyToOne(() => NotificationToken)
-  notification_token: NotificationToken;
+  notification_token: NotificationToken;*/
+
+  @ManyToOne(() => KudogUser, (user) => user.notifications, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: KudogUser;
 
   @Column()
   title: string;
 
   @Column({ type: 'longtext', nullable: true })
-  body: any;
+  body: string;
 
-  @Column({
-    default: 'ACTIVE',
-  })
-  status: string;
+  @CreateDateColumn({ name: 'created_date', type: 'timestamp' })
+  date: Date;
 
   @Column()
-  created_By: number; // 알림을 생성한 사용자의 ID
+  created_By: number;
+  // 알림을 생성한 사용자의 ID -> DB에서 조회할 때 혹시 필요할까봐 살려뒀는데 필요 없으면 지워도 돼요.
 }
