b6fbfe0f37e895bd0b6ea6747170fb79905dc2a3
overthestream <njhnjh02@naver.com>
2024-04-13 13:51:33 +0900

    시차

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,8 @@
 const DAY_IN_MILLISECONDS = 86400000;
+const HOUR_IN_MILLISECONDS = 3600000;
 const MINUTE_IN_MILLISECONDS = 60000;
 export function getHHMMdate(): string {
-  let timestamp = Date.now();
+  let timestamp = Date.now() + 9 * HOUR_IN_MILLISECONDS;
   timestamp %= DAY_IN_MILLISECONDS;
   timestamp /= MINUTE_IN_MILLISECONDS;
   const hours = Math.floor(timestamp / 60);
