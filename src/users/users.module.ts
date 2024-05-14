8f0e135771d95fa5fe345ab63cc7374fd24b61b6
daheekim01 <ekgml5334@gmail.com>
2024-05-14 18:23:09 +0900

    즐겨찾기 기능 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { KudogUser } from 'src/entities';
+import { favoriteMajor } from 'src/entities/favoriteMajor.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([KudogUser])],
+  imports: [TypeOrmModule.forFeature([KudogUser, favoriteMajor])],
 })
 export class UsersModule {}
