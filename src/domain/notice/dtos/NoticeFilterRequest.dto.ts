6f20475d82351884d1e2eb9284ff0c60e88f6061
overthestream <njhnjh02@naver.com>
2024-05-23 21:28:15 +0900

    add isoptioanl

--- src/domain/notice/dtos/NoticeFilterRequest.dto.ts
+++ src/domain/notice/dtos/NoticeFilterRequest.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsString } from 'class-validator';
+import { IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class NoticeFilterRequestDto {
   @ApiProperty({ description: '카테고리 목록', example: '공지사항' })
   @IsString({ message: '카테고리는 문자열이어야 합니다.' })
+  @IsOptional()
   categories?: string;
 
   @ApiProperty({ description: 'Provider 목록', example: '정보대학,미디어학부' })
   @IsString({ message: 'Provider는 문자열이어야 합니다.' })
+  @IsOptional()
   providers?: string;
 
   @ApiProperty({ description: '조회 시작 기간', example: '2020-01-01' })
@@ -15,6 +17,7 @@ export class NoticeFilterRequestDto {
     {},
     { message: '시작 날짜는 YYYY-MM-DD 날짜 형식이어야 합니다.' },
   )
+  @IsOptional()
   start_date?: string;
 
   @ApiProperty({ description: '조회 종료 기간', example: '2020-01-01' })
@@ -22,5 +25,6 @@ export class NoticeFilterRequestDto {
     {},
     { message: '종료 날짜는 YYYY-MM-DD 날짜 형식이어야 합니다.' },
   )
+  @IsOptional()
   end_date?: string;
 }
--- src/interfaces/pageQuery.ts
+++ src/interfaces/pageQuery.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsPositive } from 'class-validator';
+import { IsNumber, IsOptional, IsPositive } from 'class-validator';
 
 export class PageQuery {
   @ApiProperty({ description: '페이지 번호', default: 1, example: 1 })
   @IsNumber({}, { message: 'page는 숫자여야 합니다.' })
-  page: number;
+  @IsOptional()
+  page?: number = 1;
 
   @ApiProperty({
     description: '한 페이지당 record 수',
@@ -12,5 +13,6 @@ export class PageQuery {
     default: 10,
   })
   @IsPositive({ message: 'page는 숫자여야 합니다.' })
-  pageSize: number;
+  @IsOptional()
+  pageSize?: number = 10;
 }
